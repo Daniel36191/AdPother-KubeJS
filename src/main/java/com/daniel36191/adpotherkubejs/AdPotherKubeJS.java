78b954daeb1329afa289894e608aabc12980f827
Daniel36191 <dmoeller3998@gmail.com>
2026-06-06 17:38:06 -0400

    Init

---
+++ src/main/java/com/daniel36191/adpotherkubejs/AdPotherKubeJS.java
@@ -0,0 +1,8 @@
+package com.daniel36191.adpotherkubejs;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(AdPotherKubeJS.MOD_ID)
+public class AdPotherKubeJS {
+    public static final String MOD_ID = "adpotherkubejs";
+}
